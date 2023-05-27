91456faee0adf3ca7c55861436060f0236908fa9
Kavindu Deshan <kkavindu.deshan@gmail.com>
2023-05-27 16:48:44 +0530

    Added Link to Not Found Page Button

--- src/pages/session/notFound.jsx
+++ src/pages/session/notFound.jsx
@@ -1,34 +1,37 @@
-import { withStyles } from '@mui/styles';
-import React from 'react';
-import Not_Found_Image from '../../assets/img/404Image.png'; 
-import Button from '@mui/material/Button';
+import { withStyles } from "@mui/styles";
+import React from "react";
+import Not_Found_Image from "../../assets/img/404Image.png";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
-const styleSheet=()=>({
-    container:{
-        display:'flex',
-        flexDirection:'column',
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    image_container:{
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        maxWidth:'10px'
-    }
-})
+const styleSheet = () => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image_container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: "10px",
+  },
+});
 
 const NotFound = (props) => {
-    let { classes } = props;
+  let { classes } = props;
 
   return (
     <div className={classes.container}>
-        <div className={classes.image_container}>
-            <img src={Not_Found_Image} alt="Image" />
-        </div>
+      <div className={classes.image_container}>
+        <img src={Not_Found_Image} alt="Image" />
+      </div>
+      <Link to="/">
         <Button variant="contained">Back To Home</Button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default withStyles(styleSheet)(NotFound);
